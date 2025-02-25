2485ba82289de886068e5d16a6536d8b1e32eb14
Ritesh <ritesh.sargam00@gmail.com>
2025-02-25 17:06:39 +0530

    Products Fluent Validation

---
+++ BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        //ProductID
+        RuleFor(temp => temp.ProductID).NotEmpty().WithMessage("ProductID can't be blank");
+
+        //ProductName
+        RuleFor(temp => temp.ProductName).NotEmpty().WithMessage("Product Name can't be blank");
+
+        //Category
+        RuleFor(temp => temp.Category).IsInEnum().WithMessage("Category can't be blank");
+
+        //UnitPrice
+        RuleFor(temp => temp.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit Price should be between 0 to {double.MaxValue}");
+
+        //QuantityInStock
+        RuleFor(temp => temp.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity in stock should be between 0 to {int.MaxValue}");
+    }
+}
