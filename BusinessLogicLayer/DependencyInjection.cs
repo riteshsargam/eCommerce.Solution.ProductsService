ce673e0e0ba84764b6abbad9aa6dca57dbf1a5ea
Ritesh <ritesh.sargam00@gmail.com>
2025-02-25 19:58:37 +0530

    Fixing Issue to test endpoints

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -1,4 +1,7 @@
 ﻿using eCommerce.BusinessLogicLayer.Mappers;
+using eCommerce.BusinessLogicLayer.ServiceContracts;
+using eCommerce.BusinessLogicLayer.Validators;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.ProductsService.BusinessLogicLayer;
@@ -10,6 +13,10 @@ public static class DependencyInjection
         //TO DO: Add Business Logic Layer services into the IoC container
 
         services.AddAutoMapper(typeof(ProductAddRequestToProductMappingProfile).Assembly);
+
+        services.AddValidatorsFromAssemblyContaining<ProductAddRequestValidator>();
+
+        services.AddScoped<IProductsService, eCommerce.BusinessLogicLayer.Services.ProductsService>();
 
         return services;
     }
