8744a3562e423d6297f8f5b38e3a134f0f5be469
Ritesh <ritesh.sargam00@gmail.com>
2025-02-25 13:22:40 +0530

    Boilerplate Code

--- ProductsMicroService.API/Program.cs
+++ ProductsMicroService.API/Program.cs
@@ -1,6 +1,28 @@
+using eCommerce.ProductsService.DataAccessLayer;
+using eCommerce.ProductsService.BusinessLogicLayer;
+using FluentValidation.AspNetCore;
+using eCommerce.ProductsMicroService.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add DAL and BLL services
+builder.Services.AddDataAccessLayer();
+builder.Services.AddBusinessLogicLayer();
+
+builder.Services.AddControllers();
+
+//FluentValidations
+builder.Services.AddFluentValidationAutoValidation();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
